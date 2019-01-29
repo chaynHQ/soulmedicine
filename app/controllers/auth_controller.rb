f1d3ff0fe21c2d95238b9c33d1c588a10e06ef33
Fahad Iqbal <iqbal.fahad90+git@gmail.com>
2019-01-28 22:29:40 -0700

    added a way to read and decode the jwt

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,20 @@
 class AuthController < ApplicationController
   def log_in; end
 
-  def callback; end
+  def callback
+    token = params['firebase_token']
+    options = {
+      algorithm: 'RS256',
+      verify_aud: ENV['FIREBASE_PROJECT_ID'],
+      verify_iss: "https://securetoken.google.com/#{ENV['FIREBASE_PROJECT_ID']}"
+    }
+    decoded_token = JWT.decode(token, nil, true, options) do |header|
+      url = URI('https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com')
+      json = JSON.parse(Net::HTTP.get(url))
+      OpenSSL::X509::Certificate.new(json[header['kid']]).public_key
+    end
+    # puts decoded_token
+  end
 
   def logout; end
 end
