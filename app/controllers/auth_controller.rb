8d1bbf0020012eb99463f6cb2a38f5760d61af2e
Fahad Iqbal <iqbal.fahad90+git@gmail.com>
2019-02-03 12:41:00 -0700

    making the auth controller leaner

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,17 +2,7 @@ class AuthController < ApplicationController
   def log_in; end
 
   def callback
-    token = params['firebase_token']
-    options = {
-      algorithm: 'RS256',
-      verify_aud: ENV['FIREBASE_PROJECT_ID'],
-      verify_iss: "https://securetoken.google.com/#{ENV['FIREBASE_PROJECT_ID']}"
-    }
-    decoded_token = JWT.decode(token, nil, true, options) do |header|
-      url = URI('https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com')
-      json = JSON.parse(Net::HTTP.get(url))
-      OpenSSL::X509::Certificate.new(json[header['kid']]).public_key
-    end
+    decoded_token = AuthService.decode_token(params['firebase_token'])
     @user_name = decoded_token[0]['name']
     @user_id = decoded_token[0]['user_id']
     @user_email = decoded_token[0]['email']
@@ -20,7 +10,7 @@ class AuthController < ApplicationController
     user = User.where(display_name: @user_name, uid: @user_id, email: @user_email).first_or_create
     session[:user] = user.id if user
 
-    redirect_to controller: 'subscription', action: 'index'
+    render :json => session[:user]
   end
 
   def logout
