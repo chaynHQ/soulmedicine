d9bc3ba4a863b0e94d758921e26a0c1913769fa7
Jits <dev@jits.co.uk>
2019-02-19 12:44:16 +0000

    Remove CSP rule preventing third party scripts from loading
    … we need to be able to load scripts for FirebaseUI (for auth).

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -31,7 +31,5 @@ Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035' # If running webpack-dev-server in HTTPS mode
-  else
-    policy.script_src :self, :https
   end
 end
