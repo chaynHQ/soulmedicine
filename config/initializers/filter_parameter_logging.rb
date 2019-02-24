9af30185349e9a6bbb58e7090a1ffe30d3994739
Jits <dev@jits.co.uk>
2019-02-24 15:01:26 +0000

    Filter out `firebase_token` and other sensitive params in logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,17 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += %i[
+  password
+  firebase_token
+  token
+  access_token
+  auth_token
+  secret_key_base
+  secret_token
+  secret
+  session
+  cookie
+  csrf
+  salt
+]
