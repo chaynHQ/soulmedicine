857186e611c24f4f112b5b5b5078c4bb77ee7585
Fahad Iqbal <iqbal.fahad90+git@gmail.com>
2019-03-02 12:31:01 -0700

    fixing spec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,9 +2,35 @@ require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
   describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
+    before do
+      @controller.class.send :before_action, :require_authentication
+
+      expect(@controller).to receive(:current_user?).and_call_original
+    end
+    context 'when a current_user is detected' do
+      let(:user) { instance_double(User) }
+
+      before do
+        expect(@controller).to receive(:current_user)
+          .and_return(user)
+      end
+
+      it 'renders the page as expected' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+    context 'when no current_user is detected' do
+      before do
+        expect(@controller).to receive(:current_user)
+          .and_return(nil)
+      end
+
+      it 'redirects to the sign in page and sets a forwarding_url' do
+        get :index
+        expect(response).to redirect_to auth_sign_in_path
+        expect(session[:forwarding_url]).not_to be_nil
+      end
     end
   end
 end
