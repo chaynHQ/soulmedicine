4e0cbc12cba66cdec6578c5345140ab3bb5a3207
Jits <dev@jits.co.uk>
2019-04-01 13:56:54 +0100

    Subscription: users must now specify a timezone for their schedule
    In the database, we still store the data in normalised UTC – we convert back and forth between the user's timezone and UTC so the user only sees the schedule in their selected timezone.
    
    BREAKING CHANGE: we now only allow users to select **1** time slot – this makes adjusting the schedule based on the user's timezone possible (with multiple time slots there are cases where it's impossible to convert to/from the user's timezones, due to the days adjustment [forward or backwards]).
    
    KNOWN ISSUE: this doesn't handle daylight savings at all :( The worst case is that users receive emails an hour before their actual scheduled time slot (note: that since schedules are shown to users as two hours, but processed every hour, the case where daylight savings would result in it being sent one hour forward would still be okay as it's still within the advertised time slot).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_204505) do
+ActiveRecord::Schema.define(version: 2019_03_27_141648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_02_24_204505) do
     t.datetime "last_delivered_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_timezone", default: "UTC"
     t.index ["active"], name: "index_subscriptions_on_active"
     t.index ["user_id", "course_slug"], name: "index_subscriptions_on_user_id_and_course_slug", unique: true
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
