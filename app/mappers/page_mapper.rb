768eaa8f530e898f5e35bb294efe9bb87bcc73da
Jits <dev@jits.co.uk>
2019-05-19 16:13:40 +0100

    Set up meta tags
    Including for Twitter and Facebook

--- app/mappers/page_mapper.rb
+++ app/mappers/page_mapper.rb
@@ -4,5 +4,7 @@ class PageMapper
   key :slug, from: 'slug'
   key :uuid, from: 'uuid'
 
+  key :title, from: %w[content title]
+  key :description, from: %w[content description]
   key :content_html, from: %w[content content_html code]
 end
