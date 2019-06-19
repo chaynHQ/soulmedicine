f3ef80af783d55a63ad043acccdc0f3caf65944e
Jits <dev@jits.co.uk>
2019-06-19 15:28:31 +0100

    Handle full width Pages from CMS
    Page content from the CMS can be marked as `full_width = true`, in which case we should apply the `container-fluid` class on the main container.
    
    Also better handles flash messages on these pages.

--- app/mappers/page_mapper.rb
+++ app/mappers/page_mapper.rb
@@ -4,6 +4,7 @@ class PageMapper
   key :slug, from: 'slug'
   key :uuid, from: 'uuid'
 
+  key :full_width, from: %w[content full_width]
   key :title, from: %w[content title]
   key :description, from: %w[content description]
   key :content_html, from: %w[content content_html code]
