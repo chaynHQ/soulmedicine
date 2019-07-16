b85c92cc6d88e3b60eec8f0140ece9f067cfae13
Sophie Hebdidge <sophiehebdidge@sophies-mbp.home>
2019-07-16 12:21:17 +0100

    Added in cookie banner

---
+++ app/webpack/components/cookie_law/cookie_law.vue
@@ -0,0 +1,11 @@
+<template>
+  <cookie-law theme="royal--rounded"></cookie-law>
+</template>
+
+<script>
+import CookieLaw from 'vue-cookie-law';
+
+export default {
+  components: { CookieLaw }
+};
+</script>
