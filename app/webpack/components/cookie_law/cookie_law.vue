2796f9b2c25f82faad6bfc1721350da87bcc9e67
Sophie Hebdidge <sophiehebdidge@Sophies-MacBook-Pro.local>
2019-07-25 12:11:59 +0100

    Included secondary and success colours

--- app/webpack/components/cookie_law/cookie_law.vue
+++ app/webpack/components/cookie_law/cookie_law.vue
@@ -9,24 +9,3 @@ export default {
   components: { CookieLaw }
 };
 </script>
-
-<style>
-.Cookie--soulmedicine {
-  background: green;
-  color: black;
-  padding: 1.25em;
-}
-
-.Cookie__button {
-  background: #f9b632;
-  padding: 0.625em 3.125em;
-  color: #fff;
-  border-radius: 20px;
-  border: 0;
-  font-size: 1em;
-}
-
-.Cookie__button:hover {
-  background: #726c;
-}
-</style>
