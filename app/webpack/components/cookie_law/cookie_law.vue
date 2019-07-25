dc2f4cc23549af819209ab1163fe4c2bd21335b6
Sophie Hebdidge <sophiehebdidge@Sophies-MacBook-Pro.local>
2019-07-25 11:45:31 +0100

    Added in custom style for cookie banner

--- app/webpack/components/cookie_law/cookie_law.vue
+++ app/webpack/components/cookie_law/cookie_law.vue
@@ -1,5 +1,5 @@
 <template>
-  <cookie-law theme="royal--rounded"></cookie-law>
+  <cookie-law theme="soulmedicine"></cookie-law>
 </template>
 
 <script>
@@ -9,3 +9,24 @@ export default {
   components: { CookieLaw }
 };
 </script>
+
+<style>
+.Cookie--soulmedicine {
+  background: green;
+  color: black;
+  padding: 1.25em;
+}
+
+.Cookie__button {
+  background: #f9b632;
+  padding: 0.625em 3.125em;
+  color: #fff;
+  border-radius: 20px;
+  border: 0;
+  font-size: 1em;
+}
+
+.Cookie__button:hover {
+  background: #726c;
+}
+</style>
