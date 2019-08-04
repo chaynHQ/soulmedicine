a5ea683f67ec0dbeed2ee68e75306adb794a862c
Jits <dev@jits.co.uk>
2019-08-04 17:11:43 +0100

    Add a TTL to the content Redis cache
    60 min TTL by default; override by using the optional `CONTENT_CACHE_TTL_MINS` env var

--- config/initializers/storyblok_client.rb
+++ config/initializers/storyblok_client.rb
@@ -8,7 +8,9 @@ STORYBLOK_CLIENT = if ENV['CONTENT_PREVIEW_MODE'] == 'true'
                    else
                      redis_cache_url = ENV.fetch('REDIS_CACHE_URL') { raise 'REDIS_CACHE_URL missing' }
                      redis = Redis.new url: redis_cache_url
-                     cache = Storyblok::Cache::Redis.new redis: redis
+
+                     ttl = ENV.fetch('CONTENT_CACHE_TTL_MINS', 60).to_i * 60
+                     cache = Storyblok::Cache::Redis.new redis: redis, ttl: ttl
 
                      Storyblok::Client.new(
                        cache: cache,
