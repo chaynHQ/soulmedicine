f5aa1f276db67c9b052a210e3dbdfe5677a1a558
Sophie Hebdidge <sophiehebdidge@sophies-mbp.home>
2019-08-12 11:19:48 +0100

    move button into vue component

---
+++ app/webpack/components/leave_site/leave_site.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    id="leave-site"
+    class="btn btn-primary btn-lg leave-button"
+    @click="leaveSite"
+  >
+    Leave this site
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    leaveSite() {
+      window.open('http://www.comicrelief.com/', '_newtab');
+      window.location.replace(
+        'https://www.google.com/search?tbm=isch&q=cute+baby+animal+memes'
+      );
+    }
+  }
+};
+</script>
