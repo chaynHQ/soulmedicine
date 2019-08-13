692aefc646da8243ad553103e6836f9932f61502
Sophie Hebdidge <sophiehebdidge@Sophies-MacBook-Pro.local>
2019-08-13 09:32:16 +0100

    Fixes to all but JS

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,22 +1,22 @@
 class ErrorsController < ApplicationController
-  before_action :set_format
 
   def not_found
-    render(status: :not_found)
+    # render :not_found, status: :not_found, content_type: 'text/html', format: 'html'
+    respond_to do |format|
+        format.any { render 'errors/not_found.html.erb',  layout: 'errors', status: :not_found, content_type: 'text/html', :formats => :html }
+    end
   end
 
   def internal_server_error
-    render(status: :internal_server_error)
+    respond_to do |format|
+      format.any { render 'errors/internal_server_error.html.erb',  layout: 'errors', status: :internal_server_error, content_type: 'text/html', :formats => :html }
+    end
   end
 
   def forbidden
-    render(status: :forbidden)
+    respond_to do |format|
+      format.any { render 'errors/forbidden.html.erb',  layout: 'errors', status: :forbidden, content_type: 'text/html', :formats => :html }
+    end
   end
 
-  private
-
-   def set_format
-    request.format = :html
-  end
-  
 end
