edaea19615d3bfa32980d04b13a4baaaaa866788
Sophie Hebdidge <sophiehebdidge@Sophies-MacBook-Pro.local>
2019-08-13 17:19:15 +0100

    continueUrl based on hosturi

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,13 @@
 # Load the Rails application.
 require_relative 'application'
 
+host_uri = if ENV.key?('HEROKU_APP_NAME')
+             URI("https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com")
+           else
+             URI(ENV.fetch('SITE_BASE_URL'))
+           end
+
+Rails.configuration.host_uri = host_uri
+
 # Initialize the Rails application.
 Rails.application.initialize!
