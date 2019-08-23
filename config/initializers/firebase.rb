7a53a52563a3f8ee89610e7d912da7020f65f94f
Sophie Hebdidge <sophiehebdidge@sophies-mbp.home>
2019-08-23 14:07:55 +0100

    Added signin to continueurl

--- config/initializers/firebase.rb
+++ config/initializers/firebase.rb
@@ -3,6 +3,6 @@ FirebaseConfig = OpenStruct.new
 FirebaseConfig.api_key = ENV.fetch('FIREBASE_API_KEY') { raise 'FIREBASE_API_KEY missing' }
 FirebaseConfig.project_id = ENV.fetch('FIREBASE_PROJECT_ID') { raise 'FIREBASE_PROJECT_ID missing' }
 
-FirebaseConfig.continueURL = Rails.configuration.host_uri.to_s
+FirebaseConfig.continueURL = Rails.configuration.host_uri.to_s+'/auth/sign_in'
 
 FirebaseConfig.freeze
