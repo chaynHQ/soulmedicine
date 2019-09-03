1204b2d53c43cdee52529bc7b8b11637b78a07e3
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-03 14:07:14 +0100

    Moved methods to model

--- app/services/courses_service.rb
+++ app/services/courses_service.rb
@@ -27,28 +27,13 @@ class CoursesService < StoryblokService
 
     course = get course_slug
     current_lesson = course.lessons.find { |l| l.slug == lesson_slug }
-    next_lesson = get_next_lesson(course.lessons, lesson_slug)
-    previous_lesson = get_previous_lesson(course.lessons, lesson_slug)
+
+    next_lesson = course.next_lesson(lesson_slug)
+    previous_lesson = course.previous_lesson(lesson_slug)
 
     raise NotFound.new(type: 'Lessons', id: "#{course_slug}/#{lesson_slug}") if current_lesson.blank?
 
     [course, current_lesson, next_lesson, previous_lesson]
-  end
-
-  def get_next_lesson(lessons, lesson_slug)
-    next_lesson = nil
-    lessons.each_cons(2) do |x, y|
-      next_lesson =  y if x.slug == lesson_slug
-    end
-    next_lesson
-  end
-
-  def get_previous_lesson(lessons, lesson_slug)
-    previous_lesson = nil
-    lessons.each_cons(2) do |x, y|
-      previous_lesson =  x if y.slug == lesson_slug
-    end
-    previous_lesson
   end
 
   private
