cc26842876b2e32955b467aecd75245128390c1c
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-03 13:38:32 +0100

    Sign-in page created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,11 +37,12 @@ module ApplicationHelper
 
   def main_container_class
     is_courses_index = current_page? courses_path
+    is_auth_sign_in_path = current_page? auth_sign_in_path
 
     case
-    when !defined?(@page) && !is_courses_index
+    when !defined?(@page) && !is_courses_index && !is_auth_sign_in_path
       return 'container'
-    when is_courses_index
+    when is_courses_index || is_auth_sign_in_path
       return 'container_fluid'
     else
       return ['page-container', @page.full_width ? 'container-fluid' : 'container'].join(' ')
