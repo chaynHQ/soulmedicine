efb77bfd06808a6c105a4d8b543eaa78c92044bd
Jits <dev@jits.co.uk>
2019-09-16 18:58:05 +0100

    Handle all formats when rendering error pages
    Prevents error routes from raising new errors!

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,22 @@
 class ErrorsController < ApplicationController
   def not_found
-    render(status: :not_found)
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.any { render status: :not_found, plain: 'Not Found' }
+    end
   end
 
   def internal_server_error
-    render(status: :internal_server_error)
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.any { render status: :internal_server_error, plain: 'Internal Server Error' }
+    end
   end
 
   def forbidden
-    render(status: :forbidden)
+    respond_to do |format|
+      format.html { render status: :forbidden }
+      format.any { render status: :forbidden, plain: 'Forbidden' }
+    end
   end
 end
