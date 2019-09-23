3260e651026b1b16730bb8b9fcc1996d1e86ae38
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-23 17:26:54 +0100

    Authentication check moved to auth controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,17 @@
 class VotesController < ApplicationController
+
+  before_action :require_authentication
+
   def create
 
-    if !current_user?
-      flash[:alert] = 'Please sign in to vote'
-      redirect_to auth_sign_in_path
-    else
+    @votes = current_user.votes
 
-      @votes = current_user.votes
+    vote = @votes.find_or_initialize_by(course_slug: params[:course_slug])
+    vote.user_id = current_user.id
+    vote.liked = !vote.liked
+    vote.save
 
-      vote = @votes.find_or_initialize_by(course_slug: params[:course_slug])
-      vote.user_id = current_user.id
-      vote.liked = !vote.liked
-      vote.save
+    redirect_to course_path(vote.course_slug)
 
-      redirect_to course_path(vote.course_slug)
-
-    end
   end
 end
