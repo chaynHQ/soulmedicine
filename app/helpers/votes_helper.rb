2a0bd46fde117cc4d3b9492ec673b5c29f8d7590
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-23 11:23:17 +0100

    Checks if a user has voted to update heart fill

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -2,7 +2,15 @@ module VotesHelper
 
   def heart_fill
 
+    @voted = has_user_voted
     return @voted ?  'fas fa-heart' : 'far fa-heart'
+
+  end
+
+  def has_user_voted
+
+    vote = current_user.votes.where(["course_slug = ?", @course.slug]).first
+    return vote.liked ? true : false
 
   end
 
