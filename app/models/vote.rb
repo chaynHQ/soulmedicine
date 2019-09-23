836331033eddb5bdf730ca61205917d369436000
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-23 11:43:13 +0100

    rubocop fixes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   def self.get_course_vote_total(course_slug)
-    @total_votes = Vote.where(:course_slug => course_slug, :liked => true).count
+    @total_votes = Vote.where(course_slug: course_slug, liked: true).count
   end
-
 end
