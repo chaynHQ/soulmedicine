0bc26b2e4ad7c932f4aaaeec040d8911f2704219
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-23 11:25:36 +0100

    Only show count if a vote is a positive vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   def self.get_course_vote_total(course_slug)
-    @total_votes = Vote.where(:course_slug => course_slug).count
+    @total_votes = Vote.where(:course_slug => course_slug, :liked => true).count
   end
 
 end
