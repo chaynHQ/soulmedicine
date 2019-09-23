d5b370f1170c634929d30f30de3b204dc1a363e3
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-23 18:00:12 +0100

    Added uniqueness to vote table

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,10 @@
 class Vote < ApplicationRecord
   belongs_to :user
 
+  validates :course_slug,
+    presence: true,
+    uniqueness: { scope: :user_id }
+
   def self.get_course_vote_total(course_slug)
     @total_votes = Vote.where(course_slug: course_slug, liked: true).count
   end
