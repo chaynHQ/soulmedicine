6b6107155ca772ffe71fd6b27030d5912a8fc832
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-23 11:43:13 +0100

    rubocop fixes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,22 +1,19 @@
 class VotesController < ApplicationController
-
   def create
     if !current_user?
       flash[:alert] = 'Please sign in to vote'
       redirect_to auth_sign_in_path
     else
 
-    @votes = current_user.votes
+      @votes = current_user.votes
 
-    vote = @votes.find_or_initialize_by(course_slug: params[:course_slug])
-    vote.user_id = current_user.id
-    vote.liked = !vote.liked
-    vote.save
+      vote = @votes.find_or_initialize_by(course_slug: params[:course_slug])
+      vote.user_id = current_user.id
+      vote.liked = !vote.liked
+      vote.save
 
     end
 
     redirect_to course_path(vote.course_slug)
-
   end
-
 end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,17 +1,11 @@
 module VotesHelper
-
   def heart_fill
-
-    @voted = has_user_voted
-    return @voted ?  'fas fa-heart' : 'far fa-heart'
-
+    @voted = user_voted?
+    @voted ? 'fas fa-heart' : 'far fa-heart'
   end
 
-  def has_user_voted
-
-    vote = current_user.votes.where(["course_slug = ?", @course.slug]).first
-    return !vote.nil? && vote.liked ? true : false
-
+  def user_voted?
+    vote = current_user.votes.where(['course_slug = ?', @course.slug]).first
+    !vote.nil? && vote.liked ? true : false
   end
-
 end
