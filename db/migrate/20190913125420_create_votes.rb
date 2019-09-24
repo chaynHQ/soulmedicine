097015e5620e76e0cc52ebbf4fa8e7012c98057d
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-24 12:04:56 +0100

    Index on course_slug

--- db/migrate/20190913125420_create_votes.rb
+++ db/migrate/20190913125420_create_votes.rb
@@ -4,6 +4,7 @@ class CreateVotes < ActiveRecord::Migration[5.2]
       t.references :user, foreign_key: true, type: 'uuid'
       t.string :course_slug, null: false
       t.index %i[user_id course_slug], unique: true
+      t.index :course_slug
       t.timestamps
     end
   end
