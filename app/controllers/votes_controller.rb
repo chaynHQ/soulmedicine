4fe018531f18b23815bc1d6776f192fa93d4b99b
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-09-24 12:09:54 +0100

    Tidied up vote controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,13 +4,14 @@ class VotesController < ApplicationController
   before_action :find_or_initialize_vote, only: %i[create destroy]
 
   def create
+    @vote.save!
     respond_to do |format|
       format.js { redirect_to course_path(@vote.course_slug) }
     end
   end
 
   def destroy
-    @vote.destroy
+    @vote.destroy if @vote.persisted?
     respond_to do |format|
       format.js { redirect_to course_path(@vote.course_slug) }
     end
@@ -20,6 +21,5 @@ class VotesController < ApplicationController
 
   def find_or_initialize_vote
     @vote = current_user.votes.find_or_initialize_by(course_slug: params[:course_id])
-    @vote.save
   end
 end
