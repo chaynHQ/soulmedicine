f117dd3af73d4ae6a6222c3da3f79138c4a26c9d
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-10-03 08:56:41 -0500

    Updated schema & migration

--- db/migrate/20191002180118_create_backups.rb
+++ db/migrate/20191002180118_create_backups.rb
@@ -1,9 +1,8 @@
 class CreateBackups < ActiveRecord::Migration[5.2]
   def change
     create_table :backups, id: :uuid do |t|
-      t.references :user, foreign_key: true, type: 'uuid'
       t.string :key
-      t.jsonb :data
+      t.jsonb :data, null: false, default: {}
 
       t.timestamps
       t.index :key, unique: true
