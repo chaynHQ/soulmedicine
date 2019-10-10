d805d9642ce0a5179aa9eb7b03a4d2ab18e135b8
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-10-10 11:34:49 +0100

    Added logging"

--- app/services/backups_processor_service.rb
+++ app/services/backups_processor_service.rb
@@ -7,11 +7,8 @@ class BackupsProcessorService
 
       data = service.all
 
-<<<<<<< HEAD
-=======
       puts 'Data with key: ' + key.to_s + ' has ' + data['data']['stories'][0].keys.length.to_s + ' keys per story.'
 
->>>>>>> 06ff294e0a1428786f322bd8a2c406c5fe205ee3
       Backup.find_or_initialize_by(key: key).update!(
         data: data
       )
