c7dc5752349be77a3e54e8c71fa7599b377fdd39
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-10-30 18:39:56 +0000

    Fail gracefully if last_course_id doesnt exist

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,20 @@ class PagesController < ApplicationController
   def landing
     params[:id] = 'landing'
     @just_signed_in = params[:just_signed_in]
-    @previous_course = courses_service.get params[:last_course_id] if params.key?(:last_course_id)
+    @previous_course = previous_course
     show
   end
 
   def show
     @page = pages_service.get params[:id]
     render :show
+  end
+
+  private
+
+  def previous_course
+      courses_service.get params[:last_course_id] if params.key?(:last_course_id)
+    rescue CoursesService::NotFound
+      nil
   end
 end
