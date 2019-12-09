0b66a56e16ecce45b52f57eaef8f6bcffed226c5
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-12-09 15:50:11 -0500

    Email fixes

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,9 +1,13 @@
 class ReactionsController < ApplicationController
-  before_action :require_authentication
+  before_action :require_authentication, only: %i[update destroy]
   before_action :find_or_initialize_reaction, only: %i[show update destroy]
 
   def show
-    update
+    if @reaction.reaction_name == params[:reaction_name]
+      destroy
+    else
+      update
+    end
   end
 
   def update
@@ -13,22 +17,26 @@ class ReactionsController < ApplicationController
     else
       @reaction.update!(reaction_name: params[:reaction_name])
     end
-
+    puts redirect_url
     redirect_to redirect_url
   end
 
   def destroy
     @reaction.destroy if @reaction.persisted?
+    puts '******'
+    puts redirect_url
+    puts '******'
     redirect_to redirect_url
   end
 
   private
 
   def find_or_initialize_reaction
-    @reaction = current_user.note_reactions.find_or_initialize_by(course_slug: params[:course_id], lesson_slug: params[:lesson_id])
+    user = SignedGlobalID.find(params[:user_id], for: :lesson_react) || current_user
+    @reaction = user.note_reactions.find_or_initialize_by(course_slug: params[:course_id], lesson_slug: params[:lesson_id])
   end
 
   def redirect_url
-    params[:redirect].presence || course_lesson_path(@reaction.course_slug, @reaction.lesson_slug)
+    params[:redirect_url].presence || course_lesson_path(@reaction.course_slug, @reaction.lesson_slug)
   end
 end
