56b56a7ac0e1e731802975d15058175954af669b
Jits <dev@jits.co.uk>
2019-12-24 10:50:49 +0000

    Fix the Rollbar config to only look for the `ROLLBAR_ENV` value if Rollbar is enabled

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,12 +1,13 @@
 RollbarConfig = OpenStruct.new
 
+RollbarConfig.enabled = !(Rails.env.test? || Rails.env.development?)
 RollbarConfig.server_access_token = ENV['SERVER_ROLLBAR_ACCESS_TOKEN']
 RollbarConfig.client_access_token = ENV['CLIENT_ROLLBAR_ACCESS_TOKEN']
-RollbarConfig.environment = (PullRequestNumber.call || ENV.fetch('ROLLBAR_ENV'))
+RollbarConfig.environment = (PullRequestNumber.call || ENV.fetch('ROLLBAR_ENV')) if RollbarConfig.enabled
 
 Rollbar.configure do |config|
   config.access_token = RollbarConfig.server_access_token
-  config.enabled = false if Rails.env.test? || Rails.env.development?
+  config.enabled = RollbarConfig.enabled
   config.environment = RollbarConfig.environment if config.enabled
   config.exception_level_filters.merge!('ActionController::UnknownFormat' => 'warning')
 end
