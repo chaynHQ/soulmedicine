1c4a352ac792e72c3266a85f5259cd7f809e47e9
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2019-12-24 08:31:29 +0000

    Update env variable names

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,5 +1,5 @@
 Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = ENV['SERVER_ROLLBAR_ACCESS_TOKEN']
   config.enabled = false if Rails.env.test? || Rails.env.development?
   config.environment = (PullRequestNumber.call || ENV.fetch('ROLLBAR_ENV')) if config.enabled
   config.exception_level_filters.merge!('ActionController::UnknownFormat' => 'warning')
