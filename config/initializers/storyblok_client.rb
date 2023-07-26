40807ffb7c3b6264475ad6cc80acbfb7c7e7fb28
Mark Tareshawty <tarebyte@github.com>
2023-07-25 21:58:31 -0400

    Upgrade storyblok gem dependency

--- config/initializers/storyblok_client.rb
+++ config/initializers/storyblok_client.rb
@@ -2,25 +2,22 @@ storyblok_token = ENV.fetch('STORYBLOK_TOKEN') { raise 'STORYBLOK_TOKEN missing'
 
 logger = Rails.env.development? ? Rails.logger : nil
 
-STORYBLOK_CLIENT = if ENV['CONTENT_PREVIEW_MODE'] == 'true'
-                     Storyblok::Client.new(
-                       token: storyblok_token,
-                       logger: logger,
-                       log_level: Rails.logger.level,
-                       version: 'draft'
-                     )
-                   else
-                     redis_cache_url = ENV.fetch('REDIS_CACHE_URL') { raise 'REDIS_CACHE_URL missing' }
-                     redis = Redis.new url: redis_cache_url
+given_configuration = {
+  api_version: 1, # TODO: Upgrade to API version 2
+  token: storyblok_token,
+  logger: logger,
+  log_level: Rails.logger.level
+}
 
-                     ttl = ENV.fetch('CONTENT_CACHE_TTL_MINS', 60).to_i * 60
-                     cache = Storyblok::Cache::Redis.new redis: redis, ttl: ttl
+if ENV['CONTENT_PREVIEW_MODE'] != 'true'
+  redis_cache_url = ENV.fetch('REDIS_CACHE_URL') { raise 'REDIS_CACHE_URL missing' }
+  redis = Redis.new url: redis_cache_url
 
-                     Storyblok::Client.new(
-                       cache: cache,
-                       token: storyblok_token,
-                       logger: logger,
-                       log_level: Rails.logger.level,
-                       version: 'published'
-                     )
-                   end
+  ttl = ENV.fetch('CONTENT_CACHE_TTL_MINS', 60).to_i * 60
+  cache = Storyblok::Cache::Redis.new redis: redis, ttl: ttl
+
+  given_configuration[:cache] = cache
+  given_configuration[:version] = 'published'
+end
+
+STORYBLOK_CLIENT = Storyblok::Client.new(given_configuration)
