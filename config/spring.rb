4a272faafe6cf7a0b5cd38aa301847ea9a7cb33c
Mark Tareshawty <tarebyte@github.com>
2023-08-02 15:24:43 -0400

    Include .env* files to spring watcher

--- config/spring.rb
+++ config/spring.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-%w[
-  .env
-  .env.local
-  .env.test
-  .ruby-version
-  .rbenv-vars
-  tmp/restart.txt
-  tmp/caching-dev.txt
-].each { |path| Spring.watch(path) }
+Spring.watch(
+  '.env',
+  '.env.local',
+  '.env.test',
+  '.ruby-version',
+  '.rbenv-vars',
+  'tmp/restart.txt',
+  'tmp/caching-dev.txt'
+)
