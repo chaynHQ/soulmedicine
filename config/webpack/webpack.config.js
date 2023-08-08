e53eed550f22ee18201c35093ec557ed1965e36a
Mark Tareshawty <tarebyte@github.com>
2023-08-08 00:46:54 -0400

    Update webpack.config.js

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,30 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+const { generateWebpackConfig, merge } = require('shakapacker');
+const webpack = require('webpack');
 
-const webpackConfig = generateWebpackConfig()
+const webpackConfig = generateWebpackConfig();
 
-module.exports = webpackConfig
+const { VueLoaderPlugin } = require('vue-loader');
+
+const erb = require('./loaders/erb');
+const vue = require('./loaders/vue');
+
+module.exports = merge(
+  {
+    module: {
+      rules: [erb, vue]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        Popper: ['popper.js', 'default']
+      })
+    ],
+    resolve: {
+      extensions: ['.css', '.scss', '.vue']
+    }
+  },
+  webpackConfig
+);
