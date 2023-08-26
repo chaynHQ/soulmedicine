35d44aa3d4e07312d3769059d9d83b26877693e6
Mark Tareshawty <mark@marktareshawty.com>
2023-08-26 09:15:45 -0400

    Load 6.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module SoulMedicine
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.autoloader = :zeitwerk
 
     config.exceptions_app = routes
