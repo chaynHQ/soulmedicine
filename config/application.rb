d6786cf9795c0c38e4ad6c967210656dd808dec8
Mark Tareshawty <tarebyte@github.com>
2023-09-12 07:51:21 -0400

    Upgrade application to Rails 7

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ require 'action_mailer/railtie'
 # require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
 # require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -26,10 +25,13 @@ module SoulMedicine
 
     config.exceptions_app = routes
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
