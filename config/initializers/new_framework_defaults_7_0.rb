bba5457b535be856c531cba092a77d16bba36ae8
Mark Tareshawty <tarebyte@github.com>
2023-10-24 09:31:12 -0400

    Don't override ActiveSupport::TimeWithZone.name

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -32,7 +32,7 @@ Rails.application.config.action_view.button_to_generates_button_tag = true
 
 # Don't override ActiveSupport::TimeWithZone.name and use the default Ruby
 # implementation.
-# Rails.application.config.active_support.remove_deprecated_time_with_zone_name = true
+Rails.application.config.active_support.remove_deprecated_time_with_zone_name = true
 
 # Calls `Rails.application.executor.wrap` around test cases.
 # This makes test cases behave closer to an actual request or job.
