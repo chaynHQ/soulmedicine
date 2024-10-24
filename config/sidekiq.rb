8a6b4c5e265fdc93f86c20f52bd1275ab2fed8a9
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2024-10-24 21:22:39 +0200

    fix: indentation for sidekiq.rb

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -7,7 +7,7 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-      url: ENV['REDIS_URL'],
-      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    url: ENV['REDIS_URL'],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
